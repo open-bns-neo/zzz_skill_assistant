3c5100550c3769a2355571050690418087cd1c12
elseyu <elseying@gmail.com>
2024-04-29 16:23:56 +0800

    缓存路径更改

--- lib/controller/cache_manager.dart
+++ lib/controller/cache_manager.dart
@@ -6,8 +6,11 @@ import 'package:path_provider/path_provider.dart';
 class CacheManager {
   static Future<Map<String, dynamic>?> load() async {
     try {
-      final dir = await getApplicationDocumentsDirectory();
-      final file = File('${dir.path}/cache.json');
+      final dir = await getFilePath();
+      if (dir == null) {
+        return null;
+      }
+      final file = File(dir);
 
       final data = await file.readAsString();
       return jsonDecode(data);
@@ -17,9 +20,32 @@ class CacheManager {
   }
 
   static Future<void> save(Map<String, dynamic> data) async {
-    final dir = await getApplicationDocumentsDirectory();
-    final file = File('${dir.path}/cache.json');
+    final dir = await getFilePath();
+    if (dir == null) {
+      return;
+    }
+    final file = File(dir);
 
     await file.writeAsString(jsonEncode(data));
+  }
+
+  static Future<void> clear() async {
+    final dir = await getFilePath();
+    if (dir == null) {
+      return;
+    }
+    final file = File(dir);
+
+    await file.delete();
+  }
+
+  static Future<String?> getFilePath() async {
+    Directory? dir;
+    try {
+      dir = await getApplicationSupportDirectory();
+    } catch (e) {
+      return null;
+    }
+    return '${dir.path}/bns_assistant_cache.json';
   }
 }
